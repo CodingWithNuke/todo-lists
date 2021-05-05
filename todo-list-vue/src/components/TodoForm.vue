df64032d9b2d9e64fd01c3b59903e72c73f06168
ronniew <rwaebers@gmail.com>
2021-05-05 13:48:49 +0200

    ✨Created TodoForm component

---
+++ todo-list-vue/src/components/TodoForm.vue
@@ -0,0 +1,43 @@
+<template>
+  <form @submit.prevent="addNewTodo" class="mb-3">
+    <label for="newTodo" class="form-label">New Todo</label>
+    <input
+      type="text"
+      class="form-control"
+      id="newTodo"
+      placeholder="Make a Todo List"
+      v-model="newTodo"
+    />
+    <button type="submit" class="btn btn-primary mt-2">Add Todo</button>
+  </form>
+</template>
+
+<script>
+import { defineComponent, ref } from "vue";
+import { useStore } from "vuex";
+
+export default defineComponent({
+  name: "TodoForm",
+
+  setup() {
+    const store = useStore();
+
+    const newTodo = ref("");
+
+    function addNewTodo() {
+      store.dispatch("addTodo", {
+        id: store.state.todos.length,
+        title: newTodo.value,
+        completed: false,
+      });
+
+      newTodo.value = "";
+    }
+
+    return {
+      newTodo,
+      addNewTodo,
+    };
+  },
+});
+</script>
