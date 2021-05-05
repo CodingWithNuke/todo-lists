7f34dbd9405c811e904032d6c61af0e22eca3de6
ronniew <rwaebers@gmail.com>
2021-05-05 13:48:33 +0200

    ✨Added Vuex Store

---
+++ todo-list-vue/src/store/index.js
@@ -0,0 +1,55 @@
+import { createStore } from 'vuex'
+
+export default createStore({
+  state: {
+    todos: [
+      {
+        id: 0,
+        title: "Make a Todo List in Vue",
+        completed: true,
+      },
+      {
+        id: 1,
+        title: "Make a Todo List in React",
+        completed: false,
+      },
+      {
+        id: 2,
+        title: "Make a Todo List in Svelte",
+        completed: false,
+      },
+      {
+        id: 3,
+        title: "Make a Todo List in Angular",
+        completed: false,
+      }
+    ]
+  },
+  mutations: {
+    ADD_TODO(state, todo) {
+      state.todos.push(todo);
+    },
+    COMPLETE_TODO(state, id) {
+      const todo = state.todos.find(todo => todo.id == id);
+
+      todo.completed = !todo.completed
+    },
+    REMOVE_TODO(state, id) {
+      state.todos = state.todos.filter(todo => todo.id != id);
+    }
+  },
+  actions: {
+    addTodo({ commit }, todo) {
+      commit('ADD_TODO', todo);
+    },
+    completeTodo({ commit }, id) {
+      commit('COMPLETE_TODO', id);
+    },
+    removeTodo({ commit }, id) {
+      commit('REMOVE_TODO', id);
+    }
+  },
+  getters: {
+    todos: (state) => state.todos
+  }
+})
